de51a83664ae40377e671a751aac929be3dd5740
David Arellano <david@aarellano.com>
2023-10-05 23:27:03 -0600

    Create remote config

---
+++ lib/core/config/remote_config.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'remote_config.g.dart';
+
+@Riverpod(keepAlive: true)
+Dio dio(DioRef ref) {
+  return RemoteConfig.buildDioClient();
+}
+
+class RemoteConfig {
+  const RemoteConfig._();
+
+  static const baseUrl = 'https://newsapi.org/';
+  static const authorizationHeaders = {
+    'Authorization': String.fromEnvironment('API_KEY')
+  };
+
+  static Dio buildDioClient() {
+    final dio = Dio()
+      ..options = BaseOptions(
+        baseUrl: baseUrl,
+        headers: authorizationHeaders,
+      );
+    dio.interceptors.add(PrettyDioLogger());
+    return dio;
+  }
+}
