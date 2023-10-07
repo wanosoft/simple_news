aeffc06b3267b874c71b1536b757ba765231d128
David Arellano <david@aarellano.com>
2023-10-07 00:04:03 -0600

    Add articles repository tests

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'core/presentation/simple_news.dart';
 
-void main() => runApp(const SimpleNews());
+void main() => runApp(const ProviderScope(child: SimpleNews()));
---
+++ test/features/articles/domain/articles_repository_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:simple_news/features/articles/domain/articles_repository.dart';
+import 'package:simple_news/features/articles/domain/entities/article.dart';
+import 'package:simple_news/features/articles/domain/entities/source.dart';
+
+import '../../../core/mock_data.dart';
+import 'entity_factory.dart';
+
+class MockArticlesRepository extends Mock implements ArticlesRepository {}
+
+void main() {
+  group('articles repository', () {
+    late ArticlesRepository articlesRepository;
+    late ProviderContainer container;
+
+    setUp(() {
+      articlesRepository = MockArticlesRepository();
+      container = ProviderContainer(
+        overrides: [
+          articlesRepositoryProvider.overrideWithValue(articlesRepository),
+        ],
+      );
+    });
+
+    test('should get top articles by source id', () async {
+      final expected = [createArticle()];
+
+      when(() => articlesRepository.getTopArticlesBySourceId(sourceId))
+          .thenAnswer((_) async => expected);
+
+      expect(
+        container.read(getTopArticlesBySourceIdProvider(sourceId)),
+        const AsyncValue<List<Article>>.loading(),
+      );
+
+      await container.read(getTopArticlesBySourceIdProvider(sourceId).future);
+
+      expect(
+        container.read(getTopArticlesBySourceIdProvider(sourceId)).value,
+        expected,
+      );
+    });
+
+    test('should get available sources', () async {
+      final expected = [createSource(description: 'description')];
+
+      when(() => articlesRepository.getSources())
+          .thenAnswer((_) async => expected);
+
+      expect(
+        container.read(getSourcesProvider),
+        const AsyncValue<List<Source>>.loading(),
+      );
+
+      await container.read(getSourcesProvider.future);
+
+      expect(
+        container.read(getSourcesProvider).value,
+        expected,
+      );
+    });
+  });
+}
