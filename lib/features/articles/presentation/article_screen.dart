fe5006a938013a5319fee642ac57ab7771f1f36f
David Arellano <david@aarellano.com>
2023-10-10 11:48:05 -0600

    Adding article screen

---
+++ lib/features/articles/presentation/article_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../domain/entities/article.dart';
+import 'favorites_screen.dart';
+
+class ArticleScreen extends ConsumerStatefulWidget {
+  ArticleScreen(
+    this.article, {
+    super.key,
+  }) : assert(article.content != null, 'Content should not be null!');
+
+  final Article article;
+
+  String get author => article.author ?? 'Unknown Author';
+
+  @override
+  ConsumerState<ArticleScreen> createState() => _ArticleScreenState();
+}
+
+class _ArticleScreenState extends ConsumerState<ArticleScreen> {
+  late bool _isSaved = false;
+  late AsyncValue<bool> isSavedState;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    ref.watch(savedArticlesProvider).maybeWhen(
+          data: (saved) => setState(
+            () {
+              final result = saved.contains(widget.article);
+              _isSaved = result;
+            },
+          ),
+          orElse: () => setState(() => _isSaved = false),
+        );
+  }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.article.title ?? 'Unnamed article'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.article.content!),
+              const SizedBox(height: 15),
+              Text(
+                'By ${widget.author}',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_isSaved) {
+            ref
+                .read(savedArticlesProvider.notifier)
+                .removeArticle(widget.article);
+            setState(() => _isSaved = false);
+          } else {
+            ref.read(savedArticlesProvider.notifier).addArticle(widget.article);
+            setState(() => _isSaved = true);
+          }
+        },
+        child: Icon(_isSaved ? Icons.favorite : Icons.favorite_border),
+      ),
+    );
+  }
+}
---
+++ test/features/articles/presentation/article_screen_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:simple_news/features/articles/domain/entities/article.dart';
+import 'package:simple_news/features/articles/presentation/article_screen.dart';
+import 'package:simple_news/features/articles/presentation/favorites_screen.dart';
+
+import '../../../core/presentation/test_widgets_utils.dart';
+import '../domain/entity_factory.dart';
+
+class MockSavedArticles extends Mock implements SavedArticles {}
+
+void main() {
+  late final Article article;
+  late final SavedArticles savedArticles;
+
+  setUp(() {
+    article = createArticle();
+    savedArticles = MockSavedArticles();
+  });
+
+  testWidgets('article screen', (tester) async {
+    when(() => savedArticles.build()).thenAnswer(
+      (invocation) async => [createArticle(title: 'other')],
+    );
+    when(() => savedArticles.addArticle(article)).thenAnswer(
+      (invocation) async {},
+    );
+    when(() => savedArticles.removeArticle(article)).thenAnswer(
+      (invocation) async {},
+    );
+
+    await prepareAndSettle(
+      tester,
+      widget: ArticleScreen(article),
+      isConsumer: true,
+      overrides: [
+        savedArticlesProvider.overrideWith(() => SavedArticles()),
+      ],
+    );
+
+    await tester.pumpAndSettle(const Duration(seconds: 50));
+
+    expect(find.byType(ArticleScreen), findsOneWidget);
+
+    final fabFinder = find.byType(FloatingActionButton);
+    expect(fabFinder, findsOneWidget);
+    await tester.ensureVisible(fabFinder);
+
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.favorite_border), findsOneWidget);
+
+    await tester.tap(fabFinder);
+    await tester.pumpAndSettle();
+
+    expect(find.byIcon(Icons.favorite), findsOneWidget);
+    expect(find.byIcon(Icons.favorite_border), findsNothing);
+  });
+}
